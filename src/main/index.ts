b2f0f48aaa32df2533efcec06a4ae44a2f82995f
Dmytro-Fedoryshyn <dmytro.fedoryshyn@teamdev.com>
2026-07-07 19:52:28 +0300

    Flush startup main failures

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,7 +2,10 @@ import './instrument';
 import * as Sentry from '@sentry/node';
 import { Application } from './Application';
 
-void new Application().initialize().catch((err: unknown) => {
+const SENTRY_FLUSH_TIMEOUT_MS = 2000;
+
+void new Application().initialize().catch(async (err: unknown) => {
   console.error('[Application] Failed to initialize:', err);
   Sentry.captureException(err);
+  await Sentry.flush(SENTRY_FLUSH_TIMEOUT_MS);
 });
